fb6a34c410bd464b0c68baa94378ed14255f4341
Shadowind201 <142364452+Shadowind201@users.noreply.github.com>
2023-09-05 22:15:30 -0300

    Nome media

---
+++ app.js
@@ -0,0 +1,23 @@
+alert("Bem vindo") // Comentario
+
+console.log("Menssagem")
+
+/*alert(4*6)
+
+console(6*5)*/
+
+nota1 = 7 // cria uma atribuição a uma variavel
+nota2 = 8
+nota3 = 5
+nota4 = 10
+
+console.log("Media:")
+// console.log((nota1+nota2+nota3+nota4)/4) // separar contas
+
+media = (nota1+nota2+nota3+nota4)/4
+
+console.log (media)
+
+nome = prompt("Informe seu nome") 
+
+alert("Ola, "+ nome + " sua media é " + media)
